945dd916b9fc185d5c3f813408f2d74e632e3905
Klaasjan Horlings <k.horlings@gmail.com>
2017-11-17 18:40:00 +0100

    Removed build output from repo

--- build/decorators/dependency.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Adds a dependency reference to this property or method parameter.
- * @param dependency Name of the required dependency.
- */
-export declare const dependency: (dependencyName: string) => (target: object, propertyKey?: string | undefined, parameterIndex?: number | undefined) => void;
--- build/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { dependency } from "./decorators/dependency";
-export { inject } from "./decorators/inject";
-export { register } from "./decorators/register";
-export { DefaultContainer } from "./container";
